0768ab11f8b1f510c6923716dd65951171826a1d
andrer54 <andrer54@gmail.com>
2023-11-24 15:33:19 -0300

    first commit

---
+++ src/main/java/com/andrer54/login_with_google/LoginWithGoogleApplication.java
@@ -0,0 +1,23 @@
+package com.andrer54.login_with_google;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class LoginWithGoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginWithGoogleApplication.class, args);
+	}
+
+}
+
+@RestController
+class HttpController {
+	@GetMapping("/public")
+	String publicRoute(){
+		return "<h1>Rota publica</h1>";
+	}
+}
---
+++ src/test/java/com/andrer54/login_with_google/LoginWithGoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrer54.login_with_google;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginWithGoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
